d20e4f257195387ca593547d053256ef0dcae197
phantuyen206 <63437893+phantuyen206@users.noreply.github.com>
2020-04-11 14:36:32 +0700

    Update Untitled1.cpp

--- Untitled1.cpp
+++ Untitled1.cpp
@@ -2,14 +2,7 @@
 #include <stdlib.h>
 #define ZISE_ARR (10)
 int arr[ZISE_ARR]={0,1,3,5,7,9,2,4,6,8};
-
-
-
-
-
 int main(){
-    
-
     int i =0;
     for (i=0;i<ZISE_ARR;i++){
         printf("%d ",arr[i]);
