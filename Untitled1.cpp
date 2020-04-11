762ed66e4f866b0231f86de81fde129c6ad01bea
Phan tuyen <phantuyen206@gmail.com>
2020-04-11 13:22:09 +0700

    Branch01 giamDan function

--- Untitled1.cpp
+++ Untitled1.cpp
@@ -2,8 +2,22 @@
 #include <stdlib.h>
 #define ZISE_ARR (10)
 int arr[ZISE_ARR]={0,1,3,5,7,9,2,4,6,8};
-
+//giam dan
+int giamDan(int arr[]){
+    int i,j;
+    for (i=0;i<ZISE_ARR-1;i++){
+        for(j=i+1;j<ZISE_ARR;j++){
+            if(arr[i]<arr[j]){
+                int tg =arr[i];
+                arr[i]=arr[j];
+                arr[j]=tg;
+            }
+        }
+    }
+}
+//giam dan 
 int main(){
+    giamDan(arr);
     int i =0;
     for (i=0;i<ZISE_ARR;i++){
         printf("%d ",arr[i]);
