805c468d565397eefdb95487121d8c88eaac4b58
phantuyen206 <63437893+phantuyen206@users.noreply.github.com>
2020-04-13 15:17:38 +0700

    Update rebaseGitHub.cpp

--- rebaseGitHub.cpp
+++ rebaseGitHub.cpp
@@ -3,30 +3,6 @@
 #define ZISE_ARR (10)
 int arr[ZISE_ARR]={0,1,3,5,7,9,2,4,6,8};
 
-void tangDan(int arr[]){
-    int i,j;
-    for(i=0;i<SIZE_ARR-1;i++){
-        for(j=i+1;j<SIZE_ARR;j++){
-            if(arr[i]>arr[j]){
-                int temp =arr[i];
-                arr[i]=arr[j];
-                arr[j]=temp;
-            }
-        }
-    }
-}//tang dan
-void giamDan(int arr[]){
-    int i,j;
-    for(i=0;i<SIZE_ARR-1;i++){
-        for(j=i+1;j<SIZE_ARR;j++){
-            if(arr[i]<arr[j]){
-                int temp =arr[i];
-                arr[i]=arr[j];
-                arr[j]=temp;
-            }
-        }
-    }
-}//giam dan
 
 int main(){
     int i =0;
