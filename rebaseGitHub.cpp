6eed31b3e054fe7c29a38d269842bf7256e68490
Phan tuyen <phantuyen206@gmail.com>
2020-04-13 14:24:16 +0700

    add function tang dan

--- rebaseGitHub.cpp
+++ rebaseGitHub.cpp
@@ -3,6 +3,19 @@
 #define ZISE_ARR (10)
 int arr[ZISE_ARR]={0,1,3,5,7,9,2,4,6,8};
 
+void tangDan(int arr[]){
+    int i,j;
+    for (i=0;i<ZISE_ARR-1;i++){
+        for(j=i+1;j<ZISE_ARR;j++){
+            if(arr[i]>arr[j]){
+                int tmp =arr[i];
+                arr[i]=arr[j];
+                arr[j]=tmp;
+            }
+        }
+    }
+printf("ok giam dan");
+}
 
 int main(){
     int i =0;
